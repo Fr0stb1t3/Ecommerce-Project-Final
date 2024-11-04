be9f8b8d0593fcd809107ed05f28e0eeba7d1ee6
Fr0stb1t3 <monishx100@gmai.com>
2024-11-05 01:03:50 +0530

    removed unused button

--- Ecommerce/src/navpages/productInfo/ProductInfo.jsx
+++ Ecommerce/src/navpages/productInfo/ProductInfo.jsx
@@ -173,13 +173,7 @@ const ProductInfo = () => {
                                                 </button>
                                             }
                                         </div>
-                                        <div className="flex gap-4 mb-6">
-                                            <button
-                                                className="w-full px-4 py-3 text-center text-gray-100 bg-blue-600 border border-transparent dark:border-gray-700 hover:border-blue-500 hover:text-blue-700 hover:bg-blue-100 rounded-xl"
-                                            >
-                                                Buy Now
-                                            </button>
-                                        </div>
+                                        
                                     </div>
                                 </div>
                             </div>
